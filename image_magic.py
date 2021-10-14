9221c1af0576b65504366811532e784044145bcf
Noel Rebiffe <jeditwin@icloud.com>
2021-10-14 11:47:18 -0700

    Greyscale function

--- image_magic.py
+++ image_magic.py
@@ -3,13 +3,44 @@
 
 from PIL import Image
 
+def to_greyscale(pixel: tuple) -> tuple:
+    """Convert a picture into greyscale
+
+    Args:
+        pixel: a 3-tuple of ints from 0-255, e.g. (140, 120, 255)
+            represents (red, green, blue)
+
+    Returns:
+        a 3-tuple pixel (r, g, b) in greyscale
+    """
+    red, green, blue = pixel
+
+    average = int((red + green + blue) / 3)
+
+    return average, average, average
+
+def to_greyscale_luma(pixel:tuple) -> tuple:
+    """Convert to greyscale using luma algorithm
+
+    Args:
+        pixel: a 3-tuple of ints from 0-255, e.g. (140, 120, 255)
+            represents (red, green, blue)
+
+    Returns:
+        a 3-tuple pixel (r, g, b) in greyscale
+    """
+    red, green, blue = pixel
+
+    grey = int(red * 0.3 + green * 0.59 + blue * 0.11)
+
+    return grey, grey, grey
 
 # Load the image (pumpkin)
 image = Image.open('./halloween-unsplash.jpg')
 output_image = Image.open('./halloween-unsplash.jpg')
 
 # Grab pixel information
-a_pixel = image.getpixel((0, 0)) # Grab pixel (0, 0) top-left
+a_pixel = image.getpixel((0, 0))  # Grab pixel (0, 0) top-left
 
 # Iterate over every pixel
 image_width = image.width
@@ -20,23 +51,17 @@ image_height = image.height
 # Take the r, g, b, add them up and divide by 3
 # replace r g b with the average
 
-# Top to bottom
+# top to bottom
 for y in range(image_height):
     # Left to right
     for x in range(image_width):
         # Grab pixel information for THIS pixel
         pixel = image.getpixel((x, y))
 
-        # grab r g b
-        red, green, blue = pixel
-
-        # calculate the average
-        average = int((red + green + blue) / 3)
-
-        # create new pixel
-        gray_pixel = (average, average, average)
+        # call to_greyscale
+        grey_pixel = to_greyscale_luma(pixel)
 
         # put that in new image
-        output_image.putpixel((x, y), gray_pixel)
+        output_image.putpixel((x, y), grey_pixel)
 
-output_image.save('grayscale.jpg')
+output_image.save('greyscale_luma.jpg')

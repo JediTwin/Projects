2dea24eb862282fb517f7b608e0dbf847b73b7fa
Noel Rebiffe <jeditwin@icloud.com>
2021-11-18 11:07:00 -0800

    Implemented backgroundimage.jpg and refined dvdimage.png

--- dvd_screensaver.py
+++ dvd_screensaver.py
@@ -15,8 +15,8 @@ GREEN = (0, 255, 0)
 BLUE  = (0, 0, 255)
 BGCOLOUR = (100, 100, 255)
 
-SCREEN_WIDTH  = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH  = 980
+SCREEN_HEIGHT = 540
 SCREEN_SIZE   = (SCREEN_WIDTH, SCREEN_HEIGHT)
 WINDOW_TITLE  = "DVD Screensaver"
 
@@ -36,9 +36,9 @@ class Dvdimage:
         self.x, self.y = ((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
         self.width = 160
         self.height = 71
-        self.img = pygame.image.load("./images/dvdimage copy.png")
-        self.x_vel = 5
-        self.y_vel = 3
+        self.img = pygame.image.load("./images/dvdimage.png")
+        self.x_vel = 6
+        self.y_vel = 4
 
     def rect(self) -> pygame.rect:
         """Returns a pygame.rect that represents the dvd_image"""
@@ -86,6 +86,10 @@ def main() -> None:
     done = False
     clock = pygame.time.Clock()
     dvd_image = Dvdimage()
+    bg_image = pygame.image.load("./images/backgroundimage.jpg")
+
+    # Transform the background image
+    bg_image = pygame.transform.scale(bg_image, (980, 540))
 
     # ------------- MAIN LOOP
     while not done:
@@ -98,7 +102,7 @@ def main() -> None:
         print(f"x: {dvd_image.x}, y: {dvd_image.y}")
 
         # --------- DRAW THE ENVIRONMENT
-        screen.fill(BLACK)
+        screen.blit(bg_image, (0, 0))
 
         # .blit(<surface/image>, coords, )
         screen.blit(dvd_image.img, (dvd_image.x, dvd_image.y))

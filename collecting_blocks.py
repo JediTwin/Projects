71d9efc8c27bdf53cbf43394b93456566658d0d1
Noel Rebiffe <jeditwin@icloud.com>
2021-11-26 09:29:12 -0800

    Added enemy sprite and scaled up player

--- collecting_blocks.py
+++ collecting_blocks.py
@@ -44,6 +44,7 @@ class Player(pygame.sprite.Sprite):
 
         # Create the image of the block
         self.image = pygame.image.load("./images/smb_smallmario.png")
+        # Scale up player sprite
         self.image = pygame.transform.scale(self.image, (48, 64))
 
         # Based on the image, create a Rect for the block
